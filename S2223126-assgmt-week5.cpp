ad546f635036861615cd2b3075dabc668d1c34e5
Li Lingfeng <lilingfeng0408@outlook.com>
2023-10-15 19:34:47 +0800

    Update assignment

--- S2223126-assgmt-week5.cpp
+++ S2223126-assgmt-week5.cpp
@@ -7,20 +7,22 @@ void triangle()
 	double length[3];
 	cout << "输入三角形的三条边长：";
 	cin >> length[0] >> length[1] >> length[2];
+	
 	sort(length, length + 3);
 
-	double sqSum = pow(length[0], 2) + pow(length[1], 2);
-	double sqMax = pow(length[2], 2);
+	double sq12 = pow(length[0], 2) + pow(length[1], 2);
+	double sq3 = pow(length[2], 2);
 
-	// 输入和浮点数运算损失精度，引入误差范围 Epsilon
-	const double EPSILON = 1e-2;
+	// 输入精度有限，引入误差范围 Epsilon，否则不可能出现等腰直角三角形等情况
+	const double EPSILON = 0.01;
 
 	if (length[0] + length[1] <= length[2])
 	{
 		cout << "不能构成三角形" << endl;
 		return;
 	}
-	
+
+	// core algorithm
 	if (length[0] == length[1] && length[1] == length[2])
 	{
 		cout << "等边三角形" << endl;
@@ -30,16 +32,16 @@ void triangle()
 	{
 		if (length[1] == length[2])
 			cout << "等腰锐角三角形" << endl;
-		else if (abs(sqSum - sqMax) < EPSILON)
+		else if (abs(sq12 - sq3) < EPSILON)
 			cout << "等腰直角三角形" << endl;
 		else
 			cout << "等腰钝角三角形" << endl;
 	}
 	
 	else{
-		if (abs(sqSum - sqMax) < EPSILON)
+		if (abs(sq12 - sq3) < EPSILON)
 			cout << "一般直角三角形" << endl;
-		else if (sqSum > sqMax + EPSILON)
+		else if (sq12 > sq3 + EPSILON)
 			cout << "一般锐角三角形" << endl;
 		else
 			cout << "一般钝角三角形" << endl;
@@ -48,18 +50,19 @@ void triangle()
 }
 
 void dateCalc(){
-	// 2023.1.1 是星期日
+	// switch 太麻烦了，直接打表
 	int monthArr[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+	// 2023 年 1 月 1 日是周日
+	string daysArr[7] = {"周日", "周一", "周二", "周三", "周四", "周五", "周六"};
 	int month, day;
 	cout << "输入月份和日期：";
 	cin >> month >> day;
-	string daysArr[7] = {"周日", "周一", "周二", "周三", "周四", "周五", "周六"};
 	int count = 0;
 	if (month > 1)
 		for (int i = 0; i < month - 1; i++)
 			count += monthArr[i];
 	count += day;
-	cout  << "这一天是" << daysArr[count % 7 - 1] << endl;
+	cout << "这一天是" << daysArr[count % 7 - 1] << endl;
 	return;
 }
 
