a8a29376814580e12cad517f273a35c6334d50da
Li Lingfeng <lilingfeng0408@outlook.com>
2024-03-01 15:41:47 +0800

    update relation judgement fn.

--- exp-0301/main.cpp
+++ exp-0301/main.cpp
@@ -6,6 +6,47 @@
 */
 
 #include "circle.h"
-#include <iostream>
 
+const double EPSILON = 1e-6;
+
+/*
+`judgeRelation` judges the relation between two circles.
+@param c1: circle 1
+@param c2: circle 2
+@return: -2 for containing, -1 for inscribed, 0 for intersecting, 1 for circumscribed, 2 for disjoint
+*/
 int judgeRelation(Circle &c1, Circle &c2);
+
+int main()
+{
+
+}
+
+int judgeRelation(Circle &c1, Circle &c2)
+{
+	double *centerRadius1 = c1.getCenterRadius();
+	double *centerRadius2 = c2.getCenterRadius();
+	double distance = sqrt(pow(centerRadius1[0] - centerRadius2[0], 2) + pow(centerRadius1[1] - centerRadius2[1], 2));
+	double radiusSum = centerRadius1[2] + centerRadius2[2];
+	double radiusDiff = fabs(centerRadius1[2] - centerRadius2[2]);
+	if (distance < EPSILON && radiusDiff < EPSILON)
+	{
+		return -2;
+	}
+	else if (distance < radiusDiff)
+	{
+		return -1;
+	}
+	else if (fabs(distance - radiusDiff) < EPSILON)
+	{
+		return 0;
+	}
+	else if (distance < radiusSum)
+	{
+		return 1;
+	}
+	else
+	{
+		return 2;
+	}
+}
