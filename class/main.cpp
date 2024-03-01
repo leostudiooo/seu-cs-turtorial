c145334a0d06c812fadeec7df2ada5b61b87ef00
Li Lingfeng <lilingfeng0408@outlook.com>
2024-03-01 15:19:21 +0800

    add circle.cpp prototype for 24-03-01 assignment

--- class/main.cpp
+++ class/main.cpp
@@ -1,9 +1,9 @@
-#include <circleclass.h>
+#include "circleclass.h"
 #include <iostream>
 #include <math.h>
 using namespace std;
 int main() {
 	circle c;
 	c.setR(1.0);
-	cout << c.getS(); << endl;
+	cout << c.getS() << endl;
 }
