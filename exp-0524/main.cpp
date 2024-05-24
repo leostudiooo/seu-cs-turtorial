06b130464b8ab552ae128c536134e16549028b45
leostudiooo <lilingfeng0408@outlook.com>
2024-05-24 17:15:04 +0800

    Version 1a of lint

--- exp-0524/main.cpp
+++ exp-0524/main.cpp
@@ -20,42 +20,86 @@ int main()
 
 	while (getline(fin, temp_line))
 	{
-		// Single-line comment: find the first occurrence of "//", with leading characters only whitespace
-		if (
-			temp_line.find("//") != string::npos &&
-			(
-				temp_line.find("//") == temp_line.find_first_not_of(" \t") ||
-				temp_line.find("") == temp_line.find_first_not_of(" ")
-			)
-		)
+		// Comment
+		if (temp_line.find("//") != string::npos || temp_line.find("/*") != string::npos)
 		{
-			line_count++;
-			comment_count++;
-			comment_line_count++;
-			clog << "INFO\tSingle-line comment found in line " << line_count << endl;
-		}
-
-		// Multi-line comment: find the first occurrence of "/*", then read along until "*/"
-		else if (temp_line.find("/*") != string::npos)
-		{
-			line_count++;
-			comment_count++;
-			comment_line_count++;
-
-			clog << "INFO\tMulti-line comment found in line " << line_count << endl;
-
-			// Read until "*/"
-			while (temp_line.find("*/") == string::npos)
+			// Judge if the "//" or "/*" is wrapped in quotes
+			bool in_quotes = false;
+			// Check each character in the line
+			for (int i = 0; i < temp_line.size(); i++)
 			{
-				getline(fin, temp_line);
-				comment_line_count++;
+				// If the character is a quote, toggle the in_quotes flag.
+				// For example:
+				// cout << "Hello, // World!"; // This is a comment
+				//         ^quote starts    ^quote ends
+				//      in_quotes -> true  in_quotes -> false
+				if (temp_line[i] == '"')
+				{
+					in_quotes = !in_quotes;
+				}
+				// If the character is a comment, and not in quotes, count it as a comment.
+				if (temp_line[i] == '/' && (temp_line[i + 1] == '/' || temp_line[i + 1] == '*') && !in_quotes)
+				{
+					break;
+				}
+			}
+
+			// If the comment is wrapped in quotes, output the line as it is.
+			if (in_quotes)
+			{
 				line_count++;
+				fout << temp_line << endl;
+			}
+			else
+			{
+				line_count++;
+				// Inline comment
+				if (temp_line.find("//") != string::npos)
+				{
+					comment_count++;
+					comment_line_count++;
+					// Remove the inline comment
+					temp_line = temp_line.substr(0, temp_line.find("//"));
+					fout << temp_line << endl;
+				}
+				// Inline block comment
+				else if (temp_line.find("/*") != string::npos && temp_line.find("*/") != string::npos)
+				{
+					comment_count++;
+					comment_line_count++;
+					// Remove the inline block comment
+					temp_line = temp_line.substr(0, temp_line.find("/*")) + temp_line.substr(temp_line.find("*/") + 2);
+					fout << temp_line << endl;
+				}
+				// Multi-line block comment
+				else if (temp_line.find("/*") != string::npos)
+				{
+					comment_count++;
+					comment_line_count++;
+					// Remove the block comment start
+					temp_line = temp_line.substr(0, temp_line.find("/*"));
+					fout << temp_line << endl;
+					// Read along until the block comment ends
+					while (getline(fin, temp_line))
+					{
+						line_count++;
+						comment_line_count++;
+						// If the block comment ends in this line
+						if (temp_line.find("*/") != string::npos)
+						{
+							// Remove the block comment end
+							temp_line = temp_line.substr(temp_line.find("*/") + 2);
+							fout << temp_line << endl;
+							break;
+						}
+					}
+				}
 			}
 		}
 
 		else
 		{
-			line_count++;
+			line_count /* Stupid inline comment */ ++;
 			// Blank line
 			if (temp_line.empty())
 			{
@@ -69,6 +113,7 @@ int main()
 
 	cout << "Total lines: " << line_count << endl;
 	cout << "Total comments: " << comment_count << endl;
+	cout << "Total comment lines: " << comment_line_count << endl;
 	cout << "Total blank lines: " << blank_count << endl;
 
 	// Cleanup
@@ -77,27 +122,23 @@ int main()
 
 	return 0;
 }
+
 /*
 Console Output:
 INFO    Reading from example.cpp...
-INFO    Blank line found in line 4
-INFO    Blank line found in line 6
-INFO    Blank line found in line 11
-INFO    Blank line found in line 14
-INFO    Blank line found in line 18
-INFO    Comment found in line 21
-INFO    Comment found in line 23
-INFO    Comment found in line 25
-INFO    Comment found in line 34
-INFO    Comment found in line 35
-INFO    Blank line found in line 48
-INFO    Comment found in line 52
-INFO    Comment found in line 58
-INFO    Blank line found in line 62
-INFO    Blank line found in line 66
-INFO    Comment found in line 67
-INFO    Blank line found in line 70
-Total lines: 72
-Total comments: 8
-Total blank lines: 9
+INFO    Blank line found in line 5
+INFO    Blank line found in line 7
+INFO    Blank line found in line 12
+INFO    Blank line found in line 15
+INFO    Blank line found in line 20
+INFO    Blank line found in line 40
+INFO    Blank line found in line 47
+INFO    Blank line found in line 100
+INFO    Blank line found in line 114
+INFO    Blank line found in line 119
+INFO    Blank line found in line 123
+Total lines: 149
+Total comments: 26
+Total comment lines: 62
+Total blank lines: 11
 */
